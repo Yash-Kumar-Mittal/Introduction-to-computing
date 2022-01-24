45c47b63b9cd09f860b37f707cf4ef8437c24c36
Yash-Kumar-Mittal <97656216+Yash-Kumar-Mittal@users.noreply.github.com>
2022-01-24 20:09:20 +0530

    Add files via upload

---
+++ asg2_output.py
@@ -0,0 +1,43 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============================================== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Output1.py ===============================================
+The length of string is :  35
+The reversed string is :  egaugnal evitisnes esac a si nohtyP
+The new string is :  a case sensitive
+The replaced string is :  Python is object oriented language
+The index of 'a' in the given String is :  10
+The given string without white spaces is :  Pythonisacasesensitivelanguage
+
+=============================================== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Output2.py ===============================================
+Enter your Name : Yash Kumar Mittal
+Enter your SID : 21105069
+Enter your Department name : ECE
+Enter your CGPA : 9.82
+
+Hey, Yash Kumar Mittal Here!
+My SID is 21105069
+I am from ECE department and my CGPA is 9.8
+
+=============================================== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Output3.py ===============================================
+Value of a & b is :  8
+Value of a | b is :  58
+Value of a ^ b is :  50
+Left shift both a and b with 2 bits :  (224, 40)
+Right shift a with 2 bits and b with 4 bits :  (14, 0)
+
+=============================================== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Output4.py ===============================================
+Enter the first number : 45
+Enter the second number : 56
+Enter the third number : 32
+The greatest of the three numbers is :  56
+
+=============================================== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Output5.py ===============================================
+Enter the String : My name is yash
+Yes
+
+=============================================== RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Output6.py ===============================================
+Enter the length of first side of triangle : 75
+Enter the length of second side of triangle : 45
+Enter the length of third side of triangle : 54
+Yes
