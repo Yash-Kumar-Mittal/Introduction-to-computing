8cbe3172b264d9d57dbed364f78bf0a481f5109d
Yash-Kumar-Mittal <97656216+Yash-Kumar-Mittal@users.noreply.github.com>
2022-03-03 22:40:15 +0530

    Add files via upload

---
+++ asg4_output.py
@@ -0,0 +1,81 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================= RESTART: C:\Users\HP\Desktop\Assg_4\asg_4.py ============================================================
+Question 1
+Enter the number of disk in tower of Hanoi:
+3
+The Disks are numbered starting from top of the tower. 
+Steps to move all the disks from Source Tower to Destination Tower is given below
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Question 2
+Enter the number of rows for Pascal's Triangle: 5
+Using recursions to create pascal's triangle: 
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+Using loops to create pascal's triangle:
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+Question 3
+Enter first number:45
+Enter second number:6
+
+The quotient when 45 is divided by 6 is 7
+The remainder when 45 is divided by 6 is 3
+
+Que3.a
+Function is callable
+45 is Not-callable
+6 is Not-callable
+
+Que3.b
+All result values are 'non-zero'
+
+Que3.c
+Values greater than 4 are 7,5,6
+
+Que3.d
+Above result in set form is shown below:
+{5, 6, 7}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 7
+Hash value of 7 is 7
+Question4
+Name of the student is Yash Kumar Mittal and roll no. is 21105069
+Destructor Called
+Question5
+
+Employee Name is Mehak and Salary is 40000
+Employee Name is Ashok and Salary is 50000
+Employee Name is Viren and Salary is 60000
+
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000
+
+Que.5b
+Employee Viren's data has been removed.
+Question 6
+*******WELCOME TO THE FRIENDSHIP GAME*******
+
+Enter Word by George:python
+Enter Word by Barbie:typhno
+
+Friendship of George and Barbie is REAL
